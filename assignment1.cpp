9931222fc8c2cbe714a6d565a4cb8a21a3a849b7
Yersultan Medetov <y.medetov@gmail.com>
2018-09-26 15:45:37 +0600

    fix

--- assignment1.cpp
+++ assignment1.cpp
@@ -28,7 +28,6 @@ int main ( int argc, char *argv[] ) {
 	int ierr = MPI_Init ( &argc, &argv );
 	ierr = MPI_Comm_size ( MPI_COMM_WORLD, &p );
 	ierr = MPI_Comm_rank ( MPI_COMM_WORLD, &id );
-	ierr = MPI_Bcast ( &n, 1, MPI_INT, 0, MPI_COMM_WORLD );
 	doTask(n, id, p);
 
 	MPI_Finalize ( );
